1015b1a39706996e562a03a288518909a8ee6ef7
Gabriel Felipe <129997625+gabszinn@users.noreply.github.com>
2024-04-07 21:20:42 -0300

    Add files via upload

---
+++ ListaVI/Atividades/pais.java
@@ -0,0 +1,17 @@
+public class pais {
+    String nome;
+  
+    public pais(String nome) {
+      this.nome = nome;
+    }
+    
+    public String getNome() {
+      return nome;
+    }
+  
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+  
+    
+  }
